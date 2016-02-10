138d6008e662364762b41b2794377cfaf5c40589
root <root@debianIRAM>
2016-02-10 15:39:29 +0100

    dernière modif'

--- src/IRAM/FSPBundle/Controller/UserController.php
+++ src/IRAM/FSPBundle/Controller/UserController.php
@@ -8,6 +8,14 @@ use PdoFsp;
 
 class UserController extends Controller
 {
+ public function accueilAction()
+ {
+    $session = $this->get('request')->getSession();
+    $request = $this->get('request');
+    $pdo = $this->get('fsp.pdo');
+    $lesAnnonces = $pdo->getAnnonces();
+    return $this->render('FSPBundle:User:accueil.html.twig', array('lesAnnonces'=>$lesAnnonces));
+ }
 
  public function connexionAction()
  {
@@ -16,40 +24,41 @@ class UserController extends Controller
 
  public function validerconnexionAction()
  {
-$session = $this->get('request')->getSession();
-$request = $this->get('request');
-$email = $request->request->get('email');
-$mdp = $request->request->get('mdp');
-$pdo = $this->get('fsp.pdo');
-$user = $pdo->getInfosUser($email,$mdp);
-if(!is_array($user)){
-return $this->render('FSPBundle:User:connexion.html.twig',array(
- 'message'=>"Erreur d'email ou de mot de passe "));
-}
-else {
-$session->set('email',$user['email']);
-$session->set('nom',$user['nom']);
-$session->set('prenom',$user['prenom']);
-$session->set('dateNaissance',$user['dateNaissance']);
-$session->set('pays',$user['pays']);        
-return $this->render('FSPBundle:User:accueil.html.twig');
+  $session = $this->get('request')->getSession();
+  $request = $this->get('request');
+  $email = $request->request->get('email');
+  $mdp = $request->request->get('mdp');
+  $pdo = $this->get('fsp.pdo');
+  $user = $pdo->getInfosUser($email,$mdp);
+  if(!is_array($user)){
+   return $this->render('FSPBundle:User:connexion.html.twig',array(
+   'message'=>"Erreur d'email ou de mot de passe "));
   }
-}  
+  else {
+   $session->set('email',$user['email']);
+   $session->set('nom',$user['nom']);
+   $session->set('prenom',$user['prenom']);
+   $session->set('dateNaissance',$user['dateNaissance']);
+   $session->set('pays',$user['pays']);        
+   return $this->render('FSPBundle:User:accueil.html.twig');
+   }
+ }  
 
-public function deconnexionAction()
-{
-$session = $this->get('request')->getSession();
-$session->clear();
-return $this->render('FSPBundle:User:connexion.html.twig');
-}
-public function inscriptionAction()
-{
-return $this->render('FSPBundle:User:inscription.html.twig');
-}
+ public function deconnexionAction()
+ {
+  $session = $this->get('request')->getSession();
+  $session->clear();
+  return $this->render('FSPBundle:User:connexion.html.twig');
+ }
 
-public function ajouterAnnonceAction()
-{
-return $this->render('FSPBundle:User:ajouter.html.twig');
+ public function inscriptionAction()
+ {
+  return $this->render('FSPBundle:User:inscription.html.twig');
+ }
+
+ public function ajouterAnnonceAction()
+ {
+  return $this->render('FSPBundle:User:ajouter.html.twig');
  }
 
  public function modifierAnnonceAction()
@@ -60,6 +69,16 @@ return $this->render('FSPBundle:User:ajouter.html.twig');
  public function supprimerAnnonceAction()
  {
   return $this->render('FSPBundle:User:supprimer.html.twig');
+ }
+
+ public function compteAction()
+ {
+  $session = $this->get('request')->getSession();
+  $request = $this->get('request');
+  $pdo = $this->get('fsp.pdo');
+  $email = $request->request->get('email');
+  $lesAnnonces = $pdo->getAnnoncesUser($email);
+  return $this->render('FSPBundle:User:compte.html.twig',array('lesAnnonces'=>$lesAnnonces));
  }
 }
 
--- src/IRAM/FSPBundle/services/PdoFsp.php
+++ src/IRAM/FSPBundle/services/PdoFsp.php
@@ -30,18 +30,32 @@ class PdoFsp{
 	}
       
         public function getAnnonces(){
-                $req = "select annonce.titre as titre, annonce.date as date, annonce.contenu as contenu, profil.nom as nom, profil.prenom as prenom, etat.libelle as etat, theme.libelle as theme from annonce, profil, etat, theme where annonce.refidetat = etat.id and annonce.refemail = profil.email and annonce.refidtheme = theme.id";
+                $req = "select annonce.titre as titre, annonce.date as date, annonce.contenu as contenu, profil.nom as nom, profil.prenom as prenom, etat.libelle as etat, theme.libelle as theme from annonce, profil, etat, theme where annonce.refidetat = etat.id and annonce.refemail = profil.email and annonce.refidtheme = theme.id order by date desc";
                 $rs = PdoFsp::$monPdo->query($req);
                 $lesLignes = $rs->fetchAll();
                 return $lesLignes;  
         }
 
         public function getAnnoncesThemes($theme){
-                  $req = "select annonce.titre as titre, annonce.date as date, annonce.contenu as contenu, profil.nom as nom, profil.prenom as prenom, etat.libelle as etat, theme.libelle as theme from annonce, profil, etat, theme where annonce.refidetat = etat.id and annonce.refemail = profil.email and annonce.refidtheme = theme.id and theme.libelle = '$theme'";
+                  $req = "select annonce.titre as titre, annonce.date as date, annonce.contenu as contenu, profil.nom as nom, profil.prenom as prenom, etat.libelle as etat, theme.libelle as theme from annonce, profil, etat, theme where annonce.refidetat = etat.id and annonce.refemail = profil.email and annonce.refidtheme = theme.id and theme.libelle = '$theme' order by date desc";
                 $rs = PdoFsp::$monPdo->query($req);
                 $lesLignes = $rs->fetchAll();
                 return $lesLignes;
         }
+
+      	public function getAnnoncesUser($email){
+		 $req = "select annonce.titre as titre, annonce.date as date, theme.libelle as theme, etat.libelle as etat from annonce, theme, profil, etat where annonce.refemail = profil.email and profil.email = '$email' and annonce.refidtheme = theme.id and annonce.refidetat = etat.id order by date desc";
+		$rs = PdoFsp::$monPdo->query($req);
+		$lesLignes = $rs->fetchAll();
+		return $lesLignes;
+	 }
+
+	public function getCommentaireAnnonce($id){
+		$req = "select commentaire.contenu as contenu, commentaire.date as date, profil.nom as nom, profil.prenom as prenom from commentaire, profil, annonce where commentaire.refemail = profil.email and commentaire.refidannonce = annonce.id and annonce.id = '$id' order by date desc";
+		$rs = PdoFsp::$monPdo->query($req);
+		$lesLignes = $rs->fetchAll();
+		return $lesLignes;
+	}
 
 }
 ?>	
